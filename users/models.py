315622ced62f4eece6b8e70dce3c990e0041fdf9
Nikushka2024 <megrelynika@gmail.com>
2025-12-13 15:27:45 +0300

    habit model

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name="email")
     telegram_id = models.CharField(max_length=50,blank=True, null=True, verbose_name="telegram_id")
-    avatar = models.ImageField(upload_to='users/',blank=True, null=True, verbose_name="аватар")
+    avatar = models.ImageField(upload_to='users/',blank=True, null=True, verbose_name="Аватар")
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
