89ba4cd25386eeb04fb538bd6afb83360b38f737
Nikushka2024 <megrelynika@gmail.com>
2025-12-14 12:56:43 +0300

    view for register

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer
 
-# Create your views here.
+
+class RegisterAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self,request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response(
+            {'id': user.id, 'email': user.email},
+            status=status.HTTP_201_CREATED
+        )
